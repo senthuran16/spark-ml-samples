8aaa955762acc34436428d9090de71d7e941edf9
senthuran16 <senthuranrc16@gmail.com>
2022-04-13 14:13:34 +0530

    Add capability to launch a URL upon service start

--- api/src/main/java/com/lohika/morning/ml/api/ApplicationConfiguration.java
+++ api/src/main/java/com/lohika/morning/ml/api/ApplicationConfiguration.java
@@ -4,9 +4,16 @@ import com.lohika.morning.ml.spark.driver.SparkContextConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.event.EventListener;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
@@ -16,6 +23,34 @@ public class ApplicationConfiguration {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationConfiguration.class, args);
+    }
+
+    @EventListener({ApplicationReadyEvent.class})
+    public void applicationReadyEvent() {
+        System.out.println("Application started ... launching browser now");
+        browse("http://localhost:3000");
+    }
+
+    public static void browse(String url) {
+        if (Desktop.isDesktopSupported()) {
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Runtime runtime = Runtime.getRuntime();
+            try {
+//                System.out.println("Executing command for WINDOWS: " + "start " + url);
+//                runtime.exec("start " + url);
+
+                System.out.println("Executing command for MAC: " + "start " + url);
+                runtime.exec("open " + url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
